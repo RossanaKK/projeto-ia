c36a7ba932ca0ffa3a2315d05c2d29e90de9f754
Rossana <roxyslife2711@gmail.com>
2026-05-07 18:58:39 +0100

    remoção de comentários desnecessários

--- src/components/ChatScreen.tsx
+++ src/components/ChatScreen.tsx
@@ -4,7 +4,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import Header from './Header';
 import LeftMenu from './LeftMenu';
 import Footer from './Footer';
-import { auth } from '../config/firebase'; // Apenas para passar o email ao Header
+import { auth } from '../config/firebase';
 
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY || "");
 const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
