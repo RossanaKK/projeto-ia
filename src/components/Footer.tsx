3e3daa09fe4226bd16434f64c189352f39307422
Rossana <roxyslife2711@gmail.com>
2026-05-14 09:46:31 +0100

    adicionado text ao Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="text-center p-3 border-top small opacity-75" style={{ backgroundColor: 'inherit' }}>
-      &copy; 2026 | Projeto IA
+      &copy; 2026 | Projeto IA. Todos os direitos reservados.
     </footer>
   );
 }
